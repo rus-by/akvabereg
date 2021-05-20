09640a009eb4e270aeca42402b5656f460e61540
rus-by <np@parnick.ru>
2021-05-20 19:01:04 +0300

    ,

--- routes/project_routes.js
+++ routes/project_routes.js
@@ -4,7 +4,7 @@ const multerS3 = require('multer-s3');
 const s3 = new AWS.S3({
     accessKeyId: 'AKIAT77347ULQGZYKIME',
     secretAccessKey: '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',
-    region: 'us-east-1'
+    region: 'us-east-2'
   });
 const Project = require('../models/project')
 // const STATES = require('../helpers/states')
