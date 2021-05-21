c5806caced4f04acc6b9bfbf3e87bb5d1280ecb3
rus-by <np@parnick.ru>
2021-05-21 14:46:13 +0300

    m

--- index.js
+++ index.js
@@ -4,24 +4,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors')
 const app = express();
-const forceSsl = function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-        return res.redirect(['https://', req.get('Host'), req.url].join(''));
-    }
-    return next();
- };
 
- app.configure(function () {      
-    if (env === 'production') {
-        app.use(forceSsl);
-    }
-
-    // other configurations etc for express go here...
- });
-// app.get('*', function (req, res, next) {
-//     if (req.headers['x-forwarded-proto'] != 'https') res.redirect('https://akvabereg.ru' + req.url) 
-//     else next() 
-//    })
+app.get(/.*/, function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] != 'https') res.redirect('https://akvabereg.ru' + req.url) 
+    else next() 
+   })
 var session = require('express-session');
 const cookieParser = require('cookie-parser')
 const passport = require('./passport');
