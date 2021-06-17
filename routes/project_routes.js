2b8149e2c9e9d281e0ca9fa172c1cc2b5cca91fd
rus-by <np@parnick.ru>
2021-06-17 19:31:51 +0300

    make app better

--- routes/project_routes.js
+++ routes/project_routes.js
@@ -4,62 +4,61 @@ const multerS3 = require('multer-s3');
 const s3 = new AWS.S3({
     // accessKeyId: 'AKIAT77347ULQGZYKIME',
     // secretAccessKey: '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',
-    region: 'us-east-2'
-  });
+    // region: 'us-east-2'
+});
 
 const S3_BUCKET = process.env.S3_BUCKET
 const Project = require('../models/project')
-// const STATES = require('../helpers/states')
+    // const STATES = require('../helpers/states')
 const routes = express.Router()
 const multer = require('multer')
 const path = require('path')
 const fs = require('fs')
 const fsPromises = fs.promises
 
-const PROJECT_STATES ={
+const PROJECT_STATES = {
     DELETED: 0,
     CREATED: 1,
     BACKUP: 2
 }
 
 const upload = multer({
-  storage: multerS3({
-      s3: s3,
-      bucket: 'akvabereg',
-    //   bucket: S3_BUCKET,
-      acl: 'public-read-write',
-      key: function (req, file, cb) {
-          console.log(file);
-          cb(null, String(Date.now()));
-      }
-  })
+    storage: multerS3({
+        s3: s3,
+        bucket: 'akvabereg',
+        //   bucket: S3_BUCKET,
+        acl: 'public-read-write',
+        key: function(req, file, cb) {
+            console.log(file);
+            cb(null, String(Date.now()));
+        }
+    })
 });
 
-routes.get('/all', async (req, res) => {
-    const allProjects = await Project.find({status: {$eq: PROJECT_STATES.CREATED}}).populate('changes')
+routes.get('/all', async(req, res) => {
+    const allProjects = await Project.find({ status: { $eq: PROJECT_STATES.CREATED } }).populate('changes')
     console.log(req.cookies)
     res.json(allProjects)
 })
 
-routes.get('/get_img', async(req,res)=>{
+routes.get('/get_img', async(req, res) => {
     const name = req.query.name
     res.sendFile(path.join(__dirname, name));
 })
 
-routes.use(async (req,res,next)=>{
-    if(req.user){
+routes.use(async(req, res, next) => {
+    if (req.user) {
         next()
-    }
-    else{
+    } else {
         res.send(401)
     }
 })
 
 
 
-routes.post('/new_project', upload.single('file'), async (req, res) => {
+routes.post('/new_project', upload.single('file'), async(req, res) => {
     console.log('start_func')
-    try{    
+    try {
         const project = new Project({
             image: req.file.location,
             title: req.body.title,
@@ -68,25 +67,24 @@ routes.post('/new_project', upload.single('file'), async (req, res) => {
             created: new Date(),
             status: PROJECT_STATES.CREATED,
             author: req.user._id
-    })
-    await project.save()
-    res.json(project)
-    }
-       catch(error){
-           res.send(error)
+        })
+        await project.save()
+        res.json(project)
+    } catch (error) {
+        res.send(error)
         console.log(error)
-}
-        
+    }
+
 })
 
 
-routes.post('/edit', upload.single('file'), async (req, res) => {
+routes.post('/edit', upload.single('file'), async(req, res) => {
     const id = req.body.id
     const newTitle = req.body.title
     const newSubTitle = req.body.subTitle
     const newDate = req.body.date
-    const project = await Project.findOne({_id:id})
-    const changes = Object.assign([],project.changes)
+    const project = await Project.findOne({ _id: id })
+    const changes = Object.assign([], project.changes)
     project.status = PROJECT_STATES.BACKUP
     const img = req.file.location
     changes.push(id)
