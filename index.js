908698c6d3537e45df80f6f11b76544e5e28285b
rus-by <np@parnick.ru>
2021-06-17 13:21:06 +0300

    m

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors')
 const app = express();
-// app.get(/.*/, function (req, res, next) {
-//     if (req.headers['x-forwarded-proto'] != 'https') res.redirect('https://www.akvabereg.ru' + req.url) 
-//     else next() 
-//    })
+app.get(/.*/, function(req, res, next) {
+    if (req.headers['x-forwarded-proto'] != 'https') res.redirect('https://www.akvabereg.ru' + req.url)
+    else next()
+})
 const session = require('express-session');
 const cookieParser = require('cookie-parser')
 const passport = require('./passport');
